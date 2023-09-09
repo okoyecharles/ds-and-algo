43f6b601d9b417a91029e411d4f4dd2e8d93c233
Charles <okoyecharles509@gmail.com>
2023-09-09 15:06:47 +0100

    Strongly Connected Components (python)

---
+++ python/graphs/sc-components.py
@@ -0,0 +1,52 @@
+# Strongly connected components
+
+from pprint import pprint
+from index import Graph
+
+edges = [
+	('a', 'b'), ('b', 'c'), ('c', 'a'), ('b', 'd'), ('d', 'e'), ('e', 'f'), ('f', 'd'), ('g', 'f'), ('g', 'h'), ('h', 'i'), ('i', 'j'), ('j', 'g'), ('j', 'k')
+]
+
+# Graph visualization
+# a --- c     e        h --- i
+#  \   /    /   \      |     |
+#    b --- d --- f --- g --- j --- k
+
+# 4 connected components
+# expected return: [ [a, c, b], [e, d, f], [h, i, g, j], [k] ]
+
+def scc(Graph):
+	visited, stack = set(), []
+	graph = Graph.graph
+
+	def visit(node):
+		if node in visited: return
+		visited.add(node)
+		for n in graph[node]:
+			visit(n)
+		stack.append(node)
+
+	for node in graph:
+		visit(node)
+
+	visited, res = set(), []
+	reversed = Graph.reversed()
+	
+	def rvisit(node, component):
+		if node in visited: return
+		visited.add(node)
+		component.append(node)
+		for n in reversed[node]:
+			rvisit(n, component)
+
+	while len(stack) > 0:
+		node = stack.pop()
+		if node not in visited:
+			res.append([])
+			rvisit(node, res[-1])
+	
+	return res
+
+graph = Graph()
+graph.add_edges(edges)
+print(scc(graph))
