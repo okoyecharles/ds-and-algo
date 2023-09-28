bb50d252e1c68e100849dde8a5a9eafc423c3c9d
Charles <okoyecharles509@gmail.com>
2023-09-28 17:08:52 +0100

    edit: Difference Beetween Two Strings (python)

--- python/mock-interview/diff-between-two-strings.py
+++ python/mock-interview/diff-between-two-strings.py
@@ -1,42 +1,24 @@
 # Difference between two strings
 
 def diffBetweenTwoStrings(source, target):
-  """
-  @param source: str
-  @param target: str
-  @return: str[]
-  """
-  
-  # diff(index, str[], ops)
-  # table -> table
-  # recursion -> call stack, memo
-  
+  def split_prepend (str, pre):
+    chars = [c for c in str]
+    chars = list(map(lambda char: pre + char, chars))
+    return chars
+
   def diff(i, j, res, ops):
-    if i == len(source) and j == len(target):
-      return (res, ops)
-    else:
-      if i >= len(source):
-        chars = target[j:].split()
-        chars = list(map(lambda char: "+" + char, chars))
-        return (res + chars, ops + (len(target) - j))
-      if j >= len(target):
-        chars = source[i:].split()
-        chars = list(map(lambda char: "-" + char, chars))
-        return (res + chars, ops + (len(source) - i))     
-    
+    if i == len(source) and j == len(target): return (res, ops)
+    elif i >= len(source): return (res + split_prepend(target[j:], "+"), ops + (len(target) - j))
+    elif j >= len(target): return (res + split_prepend(source[i:], "-"), ops + (len(source) - i))
+
     if source[i] == target[j]:
       return diff(i + 1, j + 1, res + [source[i]], ops)
     else:
-      # add a char
       add = diff(i, j + 1, res + ['+' + target[j]], ops + 1)
-      # delete a char
       delete = diff(i + 1, j, res + ['-' + source[i]], ops + 1)
+      return add if add[1] < delete[1] else delete
       
-      if add[1] < delete[1]:
-        return add
-      else:
-        return delete
-      
-  print(diff(0, 0, [], 0))
+  chars, operations = diff(0, 0, [], 0)
+  return chars
     
 diffBetweenTwoStrings("ABCDEFG", "ABDFFGH")
